eb9981f87e01ffa580a85d5deff64a56e6d86c6e
Tomno <tomnoviv@gmail.com>
2023-03-26 13:19:08 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(event){
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = "This is really cool!";
+   });
